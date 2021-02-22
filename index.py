6483dc1964f283162fe0ced73941b907e1d2c99d
charlie <sadegh.rez@gmail.com>
2021-02-22 22:38:29 +0330

    create index

---
+++ index.py
@@ -0,0 +1,16 @@
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+pn = [1,2]
+counter = 3
+
+#while len(pn) < 10 :
+#    pass
+
+
+print(pn[-1])
+
+
